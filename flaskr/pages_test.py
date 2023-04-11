d69a73ed9929f06a27a27c0c575f020d21b9223c
Sarah Diaz <sarah_diaz@techexchange.in>
2023-04-11 03:51:13 +0000

    Modified unit tests

--- flaskr/pages_test.py
+++ flaskr/pages_test.py
@@ -341,33 +341,50 @@ def test_about(client):
         mock_get_image.assert_any_call('sarah')
         mock_get_image.assert_any_call('ricardo')
 
+
 def test_about_page_has_search_bar(client):
-    response = client.get('/about')
-    assert b'<input type="text" placeholder="Search for a PC part" name="search">' in response.data
+
+    with patch.object(backend.Backend, 'get_image') as mock_get_image:
+        mock_get_image.return_value = None
+        response = client.get('/about')
+        assert b'<input type="text" placeholder="Search for a PC part" name="search">' in response.data
+
 
 def test_home_page_has_search_bar(client):
     response = client.get('/')
     assert b'<input type="text" placeholder="Search for a PC part" name="search">' in response.data
-    
+
+
 def test_pages_page_has_search_bar(client):
-    response = client.get('/pages')
-    assert b'<input type="text" placeholder="Search for a PC part" name="search">' in response.data
+    with patch.object(backend.Backend,
+                      'get_all_page_names') as mock_get_all_page_names:
+        mock_get_all_page_names = None
+        response = client.get('/pages')
+        assert b'<input type="text" placeholder="Search for a PC part" name="search">' in response.data
+
 
 def test_uploaded_page_has_search_bar(client):
-    response = client.get('/pages/cpu.html')
-    assert b'<input type="text" placeholder="Search for a PC part" name="search">' in response.data
+
+    with patch.object(backend.Backend, 'get_wiki_page') as mock_get_wiki_page:
+        mock_get_wiki_page = None
+        response = client.get('/pages/cpu.html')
+        assert b'<input type="text" placeholder="Search for a PC part" name="search">' in response.data
+
 
 def test_upload_page_has_search_bar(client):
     response = client.get('/upload')
     assert b'<input type="text" placeholder="Search for a PC part" name="search">' in response.data
 
+
 def test_logout_page_has_search_bar(client):
     response = client.get('/logout')
     assert b'<input type="text" placeholder="Search for a PC part" name="search">' in response.data
 
+
 def test_login_page_has_search_bar(client):
     response = client.get('/login')
     assert b'<input type="text" placeholder="Search for a PC part" name="search">' in response.data
+
 
 def test_signup_page_has_search_bar(client):
     response = client.get('/signup')
